95736f512f2e50801edf586cff011fc0880148b2
quantd55 <quan.td55@gmail.com>
2020-08-04 09:06:19 +0700

    Update todo app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import TodoItem from "./components/TodoItem";
+import tickall from "./img/tickall.svg";
 
 class App extends Component {
   constructor() {
@@ -16,16 +17,30 @@ class App extends Component {
 
   onItemClick(item) {
     return (event) => {
-      console.log(item);
+      //   console.log(item);
       const isComplete = item.isComplete;
       const { todoItems } = this.state;
       const index = todoItems.indexOf(item);
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index),
+          {
+            ...item,
+            isComplete: !isComplete,
+          },
+          ...todoItems.slice(index + 1),
+        ],
+      });
     };
   }
 
   render() {
     return (
       <div className="App">
+        <div className="Header">
+          <img src={tickall} width={28} />
+          <input type="text" />
+        </div>
         {this.state.todoItems.map((item, index) => (
           <TodoItem key={index} item={item} onClick={this.onItemClick(item)} />
         ))}
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import "./TodoItem.css";
 import classNames from "classnames";
+import success from "../img/success.svg";
+import tick from "../img/tick.svg";
 
 class TodoItem extends Component {
   render() {
     const { item, onClick } = this.props;
+    let img = tick;
+    if (item.isComplete) {
+      img = success;
+    }
     return (
       <div
-        onClick={onClick}
         className={classNames("TodoItem", {
           "TodoItem-complete": item.isComplete,
         })}
       >
+        <img onClick={onClick} src={img} width={32} />
         <p>{item.title}</p>
       </div>
     );
